b0641ea2479429be0b2a6d7cf924b90dbcc42de9
jarekkopa <jarek.kopa@gmail.com>
2018-04-14 15:29:09 +0200

    Dodanie Comment i User Entity

---
+++ src/Entity/CommentEntity.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentEntityRepository")
+ */
+class CommentEntity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
