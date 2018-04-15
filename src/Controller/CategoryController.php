5bbcf4f771112e32619da6f8cf01e1a03f47f31d
jarekkopa <jarek.kopa@gmail.com>
2018-04-15 10:32:31 +0200

    Dodanie nowej Route i metody addCategory

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,15 +10,19 @@ class CategoryController extends Controller
 {
     public function showFilms($id)
     {
+        return $this->render('category/showFilms.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    public function addCategory()
+    {
         $category = new CategoryEntity();
         $category->setName('Symfony');
 
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($category);
         $entityManager->flush();
-
-        return $this->render('category/showFilms.html.twig', [
-            'controller_name' => 'CategoryController',
-        ]);
+        return $this->redirectToRoute('index');
     }
 }
